13608d2545fc1e400b754d7eff44eead371366c7
Firas Almasry <faersmasre1999@gmail.com>
2024-04-27 23:03:25 +0300

    Add Projects

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "../sub/ProjectCard";
 const Projects = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center py-10"
+      className="flex flex-col items-center justify-center py-10 z-[20]"
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
@@ -12,17 +12,40 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
+          link="https://alriyada-alebdaa.com/"
+          src="/alryada.png"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/CardImage.png"
+          link="https://tailwind-css-chi-swart.vercel.app/"
+          src="/Nike.png"
           title="Interactive Website Cards"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
+          link="https://js-css-portfolio-project-master-five.vercel.app/"
+          src="/Screenshot 2024-04-27 224730.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 my-4">
+        <ProjectCard
+          link="https://site2-sand.vercel.app/"
+          src="/temp.png"
+          title="Modern Next.js Portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          link="https://travel-delta-ruddy.vercel.app/"
+          src="/travel.png"
+          title="Interactive Website Cards"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          link="https://aaqar-plus-web-v1.vercel.app/"
+          src="/Screenshot 2024-04-27 224940.png"
           title="Space Themed Website"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
